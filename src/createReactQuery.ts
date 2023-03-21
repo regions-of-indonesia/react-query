c81d4a13c5d01809e2b743754dd29b1bb55aeac2
flamrdevs <flamrdevs@gmail.com>
2023-03-22 06:08:03 +0700

    Add name options

--- src/createReactQuery.ts
+++ src/createReactQuery.ts
@@ -5,67 +5,78 @@ import type { QueryFunctionContext as Context } from "@tanstack/react-query";
 
 type QueryKey = [string, string];
 
-const isKey = (value: unknown): value is string => typeof value === "string" && value !== "",
-  getValidKey = <T>(value: unknown, callback: (value: string) => T) => (isKey(value) ? callback(value) : ([] as unknown as QueryKey));
+const iskey = (value: unknown): value is string => typeof value === "string" && value !== "",
+  validkey = <T>(value: unknown, callback: (value: string) => T) => (iskey(value) ? callback(value) : ([] as unknown as QueryKey));
 
-const initialDataOptions = <QK, QF>(queryKey: QK, queryFn: QF, enabled?: boolean) => ({ queryKey, queryFn, enabled });
+const initial = <QK, QF>(queryKey: QK, queryFn: QF, enabled?: boolean) => ({ queryKey, queryFn, enabled });
 
-const createReactQuery = (client: RegionsOfIndonesiaClient = new RegionsOfIndonesiaClient()) => {
-  const key = {
-      provinces: ["provinces"],
-      province: (code: string) => ["province", code] as QueryKey,
-      districts: (provinceCode: string) => ["districts", provinceCode] as QueryKey,
-      district: (code: string) => ["district", code] as QueryKey,
-      subdistricts: (districtCode: string) => ["subdistricts", districtCode] as QueryKey,
-      subdistrict: (code: string) => ["subdistrict", code] as QueryKey,
-      villages: (subdistrictCode: string) => ["villages", subdistrictCode] as QueryKey,
-      village: (code: string) => ["village", code] as QueryKey,
+const createReactQuery = (client: RegionsOfIndonesiaClient = new RegionsOfIndonesiaClient(), options: { name?: string } = {}) => {
+  const { name = "regions-of-indonesia" } = options,
+    named = (value: string) => (typeof name === "string" ? [name, value].join("/") : value),
+    keyname = {
+      ps: named("provinces"),
+      p: named("province"),
+      ds: named("districts"),
+      d: named("district"),
+      ss: named("subdistricts"),
+      s: named("subdistrict"),
+      vs: named("villages"),
+      v: named("village"),
 
-      search: (name: string) => ["search", name] as QueryKey,
-      searchProvinces: (name: string) => ["search/provinces", name] as QueryKey,
-      searchDistricts: (name: string) => ["search/districts", name] as QueryKey,
-      searchSubdistricts: (name: string) => ["search/subdistricts", name] as QueryKey,
-      searchVillages: (name: string) => ["search/villages", name] as QueryKey,
+      f: named("search"),
+      fP: named("search/provinces"),
+      fD: named("search/districts"),
+      fS: named("search/subdistricts"),
+      fV: named("search/villages"),
+    },
+    key = {
+      ps: [keyname.ps],
+      p: (code: string) => [keyname.p, code] as QueryKey,
+      ds: (provinceCode: string) => [keyname.ds, provinceCode] as QueryKey,
+      d: (code: string) => [keyname.d, code] as QueryKey,
+      ss: (districtCode: string) => [keyname.ss, districtCode] as QueryKey,
+      s: (code: string) => [keyname.s, code] as QueryKey,
+      vs: (subdistrictCode: string) => [keyname.vs, subdistrictCode] as QueryKey,
+      v: (code: string) => [keyname.v, code] as QueryKey,
+
+      f: (name: string) => [keyname.f, name] as QueryKey,
+      fP: (name: string) => [keyname.fP, name] as QueryKey,
+      fD: (name: string) => [keyname.fD, name] as QueryKey,
+      fS: (name: string) => [keyname.fS, name] as QueryKey,
+      fV: (name: string) => [keyname.fV, name] as QueryKey,
     },
     fetcher = {
-      provinces: () => client.province.find(),
-      province: <C extends Context<QueryKey>>(ctx: C) => client.province.findByCode(ctx.queryKey[1]),
-      districts: <C extends Context<QueryKey>>(ctx: C) => client.district.findByProvinceCode(ctx.queryKey[1]),
-      district: <C extends Context<QueryKey>>(ctx: C) => client.district.findByCode(ctx.queryKey[1]),
-      subdistricts: <C extends Context<QueryKey>>(ctx: C) => client.subdistrict.findByDistrictCode(ctx.queryKey[1]),
-      subdistrict: <C extends Context<QueryKey>>(ctx: C) => client.subdistrict.findByCode(ctx.queryKey[1]),
-      villages: <C extends Context<QueryKey>>(ctx: C) => client.village.findBySubdistrictCode(ctx.queryKey[1]),
-      village: <C extends Context<QueryKey>>(ctx: C) => client.village.findByCode(ctx.queryKey[1]),
+      ps: () => client.province.find(),
+      p: <C extends Context<QueryKey>>(ctx: C) => client.province.findByCode(ctx.queryKey[1]),
+      ds: <C extends Context<QueryKey>>(ctx: C) => client.district.findByProvinceCode(ctx.queryKey[1]),
+      d: <C extends Context<QueryKey>>(ctx: C) => client.district.findByCode(ctx.queryKey[1]),
+      ss: <C extends Context<QueryKey>>(ctx: C) => client.subdistrict.findByDistrictCode(ctx.queryKey[1]),
+      s: <C extends Context<QueryKey>>(ctx: C) => client.subdistrict.findByCode(ctx.queryKey[1]),
+      vs: <C extends Context<QueryKey>>(ctx: C) => client.village.findBySubdistrictCode(ctx.queryKey[1]),
+      v: <C extends Context<QueryKey>>(ctx: C) => client.village.findByCode(ctx.queryKey[1]),
 
-      search: <C extends Context<QueryKey>>(ctx: C) => client.search(ctx.queryKey[1]),
-      searchProvinces: <C extends Context<QueryKey>>(ctx: C) => client.province.search(ctx.queryKey[1]),
-      searchDistricts: <C extends Context<QueryKey>>(ctx: C) => client.district.search(ctx.queryKey[1]),
-      searchSubdistricts: <C extends Context<QueryKey>>(ctx: C) => client.subdistrict.search(ctx.queryKey[1]),
-      searchVillages: <C extends Context<QueryKey>>(ctx: C) => client.village.search(ctx.queryKey[1]),
+      f: <C extends Context<QueryKey>>(ctx: C) => client.search(ctx.queryKey[1]),
+      fP: <C extends Context<QueryKey>>(ctx: C) => client.province.search(ctx.queryKey[1]),
+      fD: <C extends Context<QueryKey>>(ctx: C) => client.district.search(ctx.queryKey[1]),
+      fS: <C extends Context<QueryKey>>(ctx: C) => client.subdistrict.search(ctx.queryKey[1]),
+      fV: <C extends Context<QueryKey>>(ctx: C) => client.village.search(ctx.queryKey[1]),
     };
 
   return {
-    useProvinces: () => useQuery(initialDataOptions(key.provinces, fetcher.provinces)),
-    useProvince: (code?: string) => useQuery(initialDataOptions(getValidKey(code, key.province), fetcher.province, isKey(code))),
-    useDistricts: (provinceCode?: string) =>
-      useQuery(initialDataOptions(getValidKey(provinceCode, key.districts), fetcher.districts, isKey(provinceCode))),
-    useDistrict: (code?: string) => useQuery(initialDataOptions(getValidKey(code, key.district), fetcher.district, isKey(code))),
-    useSubdistricts: (districtCode?: string) =>
-      useQuery(initialDataOptions(getValidKey(districtCode, key.subdistricts), fetcher.subdistricts, isKey(districtCode))),
-    useSubdistrict: (code?: string) => useQuery(initialDataOptions(getValidKey(code, key.subdistrict), fetcher.subdistrict, isKey(code))),
-    useVillages: (subdistrictCode?: string) =>
-      useQuery(initialDataOptions(getValidKey(subdistrictCode, key.villages), fetcher.villages, isKey(subdistrictCode))),
-    useVillage: (code?: string) => useQuery(initialDataOptions(getValidKey(code, key.village), fetcher.village, isKey(code))),
+    useProvinces: () => useQuery(initial(key.ps, fetcher.ps)),
+    useProvince: (code?: string) => useQuery(initial(validkey(code, key.p), fetcher.p, iskey(code))),
+    useDistricts: (provinceCode?: string) => useQuery(initial(validkey(provinceCode, key.ds), fetcher.ds, iskey(provinceCode))),
+    useDistrict: (code?: string) => useQuery(initial(validkey(code, key.d), fetcher.d, iskey(code))),
+    useSubdistricts: (districtCode?: string) => useQuery(initial(validkey(districtCode, key.ss), fetcher.ss, iskey(districtCode))),
+    useSubdistrict: (code?: string) => useQuery(initial(validkey(code, key.s), fetcher.s, iskey(code))),
+    useVillages: (subdistrictCode?: string) => useQuery(initial(validkey(subdistrictCode, key.vs), fetcher.vs, iskey(subdistrictCode))),
+    useVillage: (code?: string) => useQuery(initial(validkey(code, key.v), fetcher.v, iskey(code))),
 
-    useSearch: (name?: string) => useQuery(initialDataOptions(getValidKey(name, key.search), fetcher.search, isKey(name))),
-    useSearchProvinces: (name?: string) =>
-      useQuery(initialDataOptions(getValidKey(name, key.searchProvinces), fetcher.searchProvinces, isKey(name))),
-    useSearchDistricts: (name?: string) =>
-      useQuery(initialDataOptions(getValidKey(name, key.searchDistricts), fetcher.searchDistricts, isKey(name))),
-    useSearchSubdistricts: (name?: string) =>
-      useQuery(initialDataOptions(getValidKey(name, key.searchSubdistricts), fetcher.searchSubdistricts, isKey(name))),
-    useSearchVillages: (name?: string) =>
-      useQuery(initialDataOptions(getValidKey(name, key.searchVillages), fetcher.searchVillages, isKey(name))),
+    useSearch: (name?: string) => useQuery(initial(validkey(name, key.f), fetcher.f, iskey(name))),
+    useSearchProvinces: (name?: string) => useQuery(initial(validkey(name, key.fP), fetcher.fP, iskey(name))),
+    useSearchDistricts: (name?: string) => useQuery(initial(validkey(name, key.fD), fetcher.fD, iskey(name))),
+    useSearchSubdistricts: (name?: string) => useQuery(initial(validkey(name, key.fS), fetcher.fS, iskey(name))),
+    useSearchVillages: (name?: string) => useQuery(initial(validkey(name, key.fV), fetcher.fV, iskey(name))),
   };
 };
 
--- test/regions-of-indonesia.ts
+++ test/regions-of-indonesia.ts
@@ -2,6 +2,8 @@ import { RegionsOfIndonesiaClient } from "@regions-of-indonesia/client";
 
 import { createReactQuery } from "../src";
 
-const { useProvinces } = createReactQuery(new RegionsOfIndonesiaClient({ baseURL: { dynamic: "http://127.1.0.0:8000" } }));
+const { useProvinces, useDistricts, useSubdistricts, useVillages } = createReactQuery(
+  new RegionsOfIndonesiaClient({ baseURL: { dynamic: "http://127.1.0.0:8000" } })
+);
 
-export { useProvinces };
+export { useProvinces, useDistricts, useSubdistricts, useVillages };
